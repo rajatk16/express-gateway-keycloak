753b19230b313be6261df7feadd6ba4f1c0e0fff
gitfish <mfisher.au@gmail.com>
2019-01-10 16:16:34 +1100

    Minor progress and cleanup.

--- KeycloakPlugin.ts
+++ KeycloakPlugin.ts
@@ -1,17 +1,24 @@
 import * as eg from "express-gateway";
 import * as express from "express";
 import * as Keycloak from "keycloak-connect";
-const session = require("express-session");
+import * as session from "express-session";
+import * as me from "memorystore";
+
+const MemoryStore = me(session);
 
 const KeycloakPlugin : ExpressGateway.Plugin = {
     version: "1.2.0",
     init: (ctx : ExpressGateway.PluginContext) => {
-        const memoryStore = new session.MemoryStore();
-        const keycloak = new Keycloak({ store: memoryStore });
+        const sessionStore = new MemoryStore();
+        const keycloak = new Keycloak({ store: sessionStore });
+        keycloak.authenticated = (req) => {
+            console.log("-- Keycloak Authenticated: " + JSON.stringify(req.kauth.grant.access_token.content));
+        };
+        
         // setup our keycloak middleware
         ctx.registerGatewayRoute(app => {
             // TODO: the secret from some config
-            app.use(session({ secret: "dunno", cookie: { maxAge: 60000 }}));
+            app.use(session({ store: sessionStore, secret: "dunno" }));
             app.use(keycloak.middleware());
         });
         ctx.registerPolicy({
