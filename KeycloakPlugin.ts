7809f498c1b2738d1f7c549ee76b37192a58314a
gitfish <mfisher.au@gmail.com>
2019-01-14 14:24:16 +1100

    Cleanup and addition of schemas to plugin and policy.

--- KeycloakPlugin.ts
+++ KeycloakPlugin.ts
@@ -2,27 +2,40 @@ import * as eg from "express-gateway";
 import * as express from "express";
 import * as Keycloak from "keycloak-connect";
 import * as session from "express-session";
-import * as me from "memorystore";
+import * as createMemoryStore from "memorystore";
 
-const MemoryStore = me(session);
+const MemoryStore = createMemoryStore(session);
 
-const KeycloakPlugin : ExpressGateway.Plugin = {
+interface IKeycloakPlugin extends ExpressGateway.Plugin {
+    sessionSecret: string;
+}
+
+const KeycloakPlugin : IKeycloakPlugin = {
     version: "1.2.0",
+    policies: ["keycloak"],
+    sessionSecret: "kc_secret",
     init: (ctx : ExpressGateway.PluginContext) => {
         const sessionStore = new MemoryStore();
         const keycloak = new Keycloak({ store: sessionStore });
-        keycloak.authenticated = (req) => {
-            console.log("-- Keycloak Authenticated: " + JSON.stringify(req.kauth.grant.access_token.content));
-        };
-        
         // setup our keycloak middleware
         ctx.registerGatewayRoute(app => {
             // TODO: the secret from some config
-            app.use(session({ store: sessionStore, secret: "dunno" }));
+            app.use(session({ store: sessionStore, secret: KeycloakPlugin.sessionSecret }));
             app.use(keycloak.middleware());
         });
         ctx.registerPolicy({
             name: "keycloak",
+            schema: {
+                type: "object",
+                properties: {
+                    role: {
+                        title: "role",
+                        description: "the keycloak role to restrict access to",
+                        type: "string",
+                        required: false
+                    }
+                }
+            },
             policy: (actionParams : any) : express.RequestHandler => {
                 return keycloak.protect(actionParams.role);
             }
