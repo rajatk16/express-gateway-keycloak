42d83d60ef0e225ebb67fb61e0c8d4e843b4de9f
Rajat S <rajat.sudagade@gmail.com>
2020-06-02 23:43:21 -0500

    added support for bearer token method of authentication

--- KeycloakPlugin.ts
+++ KeycloakPlugin.ts
@@ -1,50 +1,52 @@
-import * as eg from 'express-gateway';
+import * as eg from 'express-gateway'
 //@ts-ignore
-import { createLoggerWithLabel } from 'express-gateway/lib/logger';
-import * as Keycloak from 'keycloak-connect';
-import * as session from 'express-session';
-import * as createMemoryStore from 'memorystore';
+import { createLoggerWithLabel } from 'express-gateway/lib/logger'
+import * as Keycloak from 'keycloak-connect'
+import * as session from 'express-session'
+import * as createMemoryStore from 'memorystore'
 
 interface KeycloakPluginSettings {
-  session: session.SessionOptions;
-  keycloakConfig: object;
-  paths: string[];
-  registerName: string;
+  session: session.SessionOptions
+  keycloakConfig: object
+  paths: string[]
+  registerName: string
 }
 interface ActionParams {
-  jsProtect?: string;
-  jsProtectTokenVar?: string;
-  role?: string;
+  jsProtect?: string
+  jsProtectTokenVar?: string
+  role?: string
 }
-const memoryStore = createMemoryStore(session);
+const memoryStore = createMemoryStore(session)
 
 const DEFAULT_KEYCLOAK_PLUGIN_SETTINGS: KeycloakPluginSettings = {
   session: {
     secret: 'kc_secret',
+    resave: false,
+    saveUninitialized: true,
   },
   keycloakConfig: {},
   registerName: 'keycloak-protect',
   paths: ['/'],
-};
+}
 
 const keycloakPlugin: eg.ExpressGateway.Plugin = {
   version: '1.2.0',
   init: (ctx: eg.ExpressGateway.PluginContext) => {
     // this is slightly dodgy casting, as they don't expose settings on the public interface - but not sure how else you can access custom settings for a plugin
-    const sessionStore = new memoryStore();
+    const sessionStore = new memoryStore()
 
     const rawSettings: KeycloakPluginSettings =
       //@ts-ignore
-      (ctx as eg.ExpressGateway.PluginContext).settings;
+      (ctx as eg.ExpressGateway.PluginContext).settings
     const sessionSettings = {
       ...DEFAULT_KEYCLOAK_PLUGIN_SETTINGS.session,
       ...rawSettings.session,
       store: sessionStore,
-    };
+    }
     const keycloakConfig = {
       ...DEFAULT_KEYCLOAK_PLUGIN_SETTINGS.keycloakConfig,
       ...rawSettings.keycloakConfig,
-    };
+    }
     const pluginSettings: KeycloakPluginSettings = {
       //@ts-ignore
       session: sessionSettings,
@@ -53,46 +55,46 @@ const keycloakPlugin: eg.ExpressGateway.Plugin = {
         rawSettings.registerName ||
         DEFAULT_KEYCLOAK_PLUGIN_SETTINGS.registerName,
       paths: rawSettings.paths || DEFAULT_KEYCLOAK_PLUGIN_SETTINGS.paths,
-    };
+    }
     const keycloak = new Keycloak(
       { store: sessionStore },
-      pluginSettings.keycloakConfig
-    );
+      pluginSettings.keycloakConfig,
+    )
     const logger = createLoggerWithLabel(
-      '[EG:plugin:' + pluginSettings.registerName + ']'
-    );
+      '[EG:plugin:' + pluginSettings.registerName + ']',
+    )
 
-    logger.debug('Init', pluginSettings.registerName);
+    logger.debug('Init', pluginSettings.registerName)
     logger.info(
       `Initialized Keycloak Plugin with settings: ${JSON.stringify(
         pluginSettings,
         null,
-        '\t'
-      )}`
-    );
+        '\t',
+      )}`,
+    )
 
-    keycloak.authenticated = req => {
-      const keyR = req as Keycloak.GrantedRequest;
-      const grant = keyR.kauth.grant as Keycloak.Grant;
+    keycloak.authenticated = (req) => {
+      const keyR = req as Keycloak.GrantedRequest
+      const grant = keyR.kauth.grant as Keycloak.Grant
       logger.info(
         '-- Keycloak Authenticated: ' +
-          JSON.stringify(grant.access_token.content, null, '\t')
-      );
-    };
+          JSON.stringify(grant.access_token.content, null, '\t'),
+      )
+    }
 
     keycloak.accessDenied = (req, res) => {
-      logger.debug('Denied', res);
-      logger.warn('-- Keycloak Access Denied.');
-      res.status(403).end('Access Denied');
-    };
+      logger.debug('Denied', res)
+      logger.warn('-- Keycloak Access Denied.')
+      res.status(403).end('Access Denied')
+    }
 
     // setup our keycloak middleware
-    ctx.registerGatewayRoute(app => {
-      logger.debug('Register', ctx);
-      logger.info('Registering Keycloak Middleware');
-      app.use(pluginSettings.paths, session(pluginSettings.session));
-      app.use(pluginSettings.paths, keycloak.middleware());
-    });
+    ctx.registerGatewayRoute((app) => {
+      logger.debug('Register', ctx)
+      logger.info('Registering Keycloak Middleware')
+      app.use(pluginSettings.paths, session(pluginSettings.session))
+      app.use(pluginSettings.paths, keycloak.middleware())
+    })
 
     ctx.registerPolicy({
       name: pluginSettings.registerName,
@@ -116,23 +118,26 @@ const keycloakPlugin: eg.ExpressGateway.Plugin = {
         },
       },
       policy: (actionParams: ActionParams) => {
-        logger.debug('policy', pluginSettings.registerName);
+        logger.debug('policy', pluginSettings.registerName)
         logger.info(
-          `-- Keycloak Protect: ${JSON.stringify(actionParams, null, '\t')}`
-        );
+          `-- Keycloak Protect: ${JSON.stringify(actionParams, null, '\t')}`,
+        )
+        if (pluginSettings.keycloakConfig['bearer-only']) {
+          return keycloak.protect()
+        }
         if (actionParams.jsProtect) {
           return keycloak.protect((token: Keycloak.Token, req) => {
             //@ts-ignore
-            req.egContext[actionParams.jsProtectTokenVar || 'token'] = token;
+            req.egContext[actionParams.jsProtectTokenVar || 'token'] = token
             //@ts-ignore
-            const runResult = req.egContext.run(actionParams.jsProtect);
-            logger.info('-- Keycloak Protect JS Result: ' + runResult);
-            return runResult;
-          });
+            const runResult = req.egContext.run(actionParams.jsProtect)
+            logger.info('-- Keycloak Protect JS Result: ' + runResult)
+            return runResult
+          })
         }
-        return keycloak.protect(actionParams.role);
+        return keycloak.protect(actionParams.role)
       },
-    });
+    })
   },
   schema: {
     $id: 'http://express-gateway.io/schemas/plugin/keycloak.json',
@@ -161,11 +166,11 @@ const keycloakPlugin: eg.ExpressGateway.Plugin = {
       },
     },
   },
-};
+}
 
 export {
   KeycloakPluginSettings as IKeycloakPluginSettings,
   DEFAULT_KEYCLOAK_PLUGIN_SETTINGS as DefaultKeycloakPluginSettings,
   keycloakPlugin as KeycloakPlugin,
   keycloakPlugin as default,
-};
+}
